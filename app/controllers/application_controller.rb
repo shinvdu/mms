48c860a1d77cb2fa65cff3a7b8a2d448931e2607
silas <xie.tianbao44@gmail.com>
2015-04-20 17:40:33 +0800

    basice usefull function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,45 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+      # for seo 
+    def set_seo_meta(title = '',meta_keywords = '', meta_description = '')
+        if title.length > 0
+            @page_title = "#{title}"
+        end
+        @meta_keywords = meta_keywords
+        @meta_description = meta_description
+    end
+     # for user can go back
+     def store_location
+     	session[:return_to] = request.original_url
+     end
+
+    # back to default
+    def redirect_back_or_default(default)
+    	redirect_to(session[:return_to] || default)
+    	session[:return_to] = nil
+    end
+
+    # go to referer
+    def redirect_referrer_or_default(default)
+    	redirect_to(request.referrer || default)
+    end
+
+    # add notice message
+    def notice_info(msg)
+    	flash[:info] = msg
+    end
+    def notice_success(msg)
+    	flash[:success] = msg
+    end
+
+    def notice_warning(msg)
+    	flash[:warning] = msg
+    end
+
+    def notice_error(msg)
+    	flash[:danger] = msg
+    end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+	def render_page_title
+		site_name = Settings.title
+		title = @page_title ? "#{@page_title} | #{site_name} " : site_name rescue "SITE_NAME"
+		content_tag("title", title, nil, false)
+	end
+	
+	def notice_message
+		flash_messages = []
+		flash.each do |type, message|
+			 type = :success if type.to_sym == :notice
+			text = content_tag(:div, message, :class => "alert alert-#{type}")
+			flash_messages << text if message
+		  end
+		  flash_messages.join("\n").html_safe
+	end
+
+
 end
