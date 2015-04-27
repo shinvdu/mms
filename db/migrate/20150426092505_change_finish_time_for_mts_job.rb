47cc4cdff241e56450a42e1132db10605a7e0eb3
silas <xie.tianbao44@gmail.com>
2015-04-27 17:32:29 +0800

    timestamp to datetime

--- db/migrate/20150426092505_change_finish_time_for_mts_job.rb
+++ db/migrate/20150426092505_change_finish_time_for_mts_job.rb
@@ -1,5 +1,5 @@
 class ChangeFinishTimeForMtsJob < ActiveRecord::Migration
   def change
-    change_column :mts_jobs, :finish_time,  :timestamp
+    change_column :mts_jobs, :finish_time,  :datetime
   end
 end
