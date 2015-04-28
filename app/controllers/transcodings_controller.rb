abdc7971c3a63f82864f05bb3edbfa8b27497a13
silas <xie.tianbao44@gmail.com>
2015-04-28 18:02:37 +0800

    quick fix

--- app/controllers/transcodings_controller.rb
+++ app/controllers/transcodings_controller.rb
@@ -84,6 +84,6 @@ class TranscodingsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def transcoding_params
-    params.require(:transcoding).permit(:name, :user_id, :container, :video_profile, :video_preset, :audio_codec, :audio_samplerate, :audio_bitrate, :video_line_scan, :h_w_percent, :width, :height, :data, :video_codec, :video_bitrate, :video_crf, :video_fps, :video_gop, :video_scanmode, :video_bufsize, :video_bitratebnd, :audio_channels, :state, :aliyun_template_id, :created_at, :updated_at)
+    params.require(:transcoding).permit(:name, :user_id, :container, :video_profile, :video_preset, :audio_codec, :audio_samplerate, :audio_bitrate, :video_line_scan, :h_w_percent, :width, :height, :data, :video_codec, :video_bitrate, :video_crf, :video_fps, :video_gop, :video_scanmode, :video_bufsize, :video_bitratebnd, :audio_channels, :state, :aliyun_template_id, :created_at, :updated_at, :video_maxrate, :video_bitrate_bnd_max,, :video_bitrate_bnd_min)
   end
 end
