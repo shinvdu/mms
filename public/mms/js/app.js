8b075a13132771981fa2a83dabb2b3feb322fb55
charliestras <charliestras@gmail.com>
2015-05-06 21:32:47 +0800

    修改form表单的样式

--- public/mms/js/app.js
+++ public/mms/js/app.js
@@ -20,6 +20,37 @@ var main = function() {
 	e.preventDefault();
     });
 
+    $('#btn-add-permission-control').on('click', function(e) {
+	$('.tab-panel').removeClass('current');
+	$('#add-permission-control').addClass('current');
+	e.preventDefault();
+    });
+
+    $('#btn-add-user').on('click', function(e) {
+	$('.tab-panel').removeClass('current');
+	$('#add-user').addClass('current');
+	e.preventDefault();
+    });
+
+    $('#btn-add-transcoding-template').on('click', function(e) {
+	$('.tab-panel').removeClass('current');
+	$('#add-transcoding-template').addClass('current');
+	e.preventDefault();
+    });
+
+    $('#btn-add-transcoding-solution').on('click', function(e) {
+	$('.tab-panel').removeClass('current');
+	$('#add-transcoding-solution').addClass('current');
+	e.preventDefault();
+    });
+
+    $('.btn-return-link').on('click', function(e) {
+	var returnID = $(this).attr('href');
+	$('.tab-panel').removeClass('current');
+	$(returnID).addClass('current');
+	e.preventDefault();
+    });	
+
     $('#btn-add-new-player').on('click', function(e) {
 	$('.tab-panel').removeClass('current');
 	$('#add-new-player').addClass('current');
