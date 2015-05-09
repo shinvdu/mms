8d1cfedfeeb7d0515c7dddc4bd1c6a04458e23f7
charliestras <charliestras@gmail.com>
2015-05-09 14:56:37 +0800

    添加用户设置页面及用户下拉菜单

--- public/mms/js/dev.js
+++ public/mms/js/dev.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
-    $('#header').load('header.html');
+    $('#header').load('header.html', function() {
+	$('#user-name').on('click', function(e) {
+	    $('#user-menu').toggle();
+	    e.preventDefault();
+	});
+
+/*
+	$('#user-name').on('blur', function(e) {
+	    $('#user-menu').hide();
+	    e.preventDefault();
+	});
+*/
+    });
     $('#footer').load('footer.html');
 });
