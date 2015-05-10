f827cc17a00a04018d79a1bb973c1c1c58ff0927
silas <xie.tianbao44@gmail.com>
2015-05-10 16:40:34 +0800

    记录广告视频

--- app/models/advertise/resource.rb
+++ app/models/advertise/resource.rb
@@ -1,4 +1,5 @@
 class Advertise::Resource < ActiveRecord::Base
   belongs_to :user
+  belongs_to :video_detail, :class_name => 'VideoDetail', :foreign_key => :advertise_resource_id
   mount_uploader :uri, AdResourceUploader
 end
