3b6e58762c25b644086c25ada8382f9139e5f52a
silas <xie.tianbao44@gmail.com>
2015-05-12 15:40:41 +0800

    基本截取

--- public/mms/js/app.js
+++ public/mms/js/app.js
@@ -4,7 +4,55 @@ var main = function() {
     var currentPanel = document.location.hash,
 	options = {hidden:false},
 	mplayer = videojs("video-display");
-    mplayer.rangeslider(options);
+	mplayer.rangeslider(options);
+
+	// var cut_hash = {
+	// 	2: {
+	// 		'dom':  
+	// 		'time': [2, 4]
+	// 	}
+	// };
+	// var active = 2;
+
+	$(".video-clip-pill").bind('click', function(e){
+		set_selection($(this));
+	})
+	$("#btn-add-clip").bind('click', function(){
+		get_new_cut_element();
+	})
+
+	function in_selected(start_time, hash){
+		// $('#video_cut_list').append();
+	}
+
+	function get_new_cut_element(){
+		$(".cut_active").removeClass('cut_active');
+		var time = mplayer.getValueSlider();
+		var start = Math.floor(time.start);
+		var end = Math.floor(time.end);
+		var dom_head = $('<div></div>').addClass('video-clip-pill').addClass('cut_active').attr('data-start', start).attr('data-end', end).append($('<input type="checkbox">'));
+		var dom_sub = $('<div></div>');
+		var dom_start = $('<div></div>').addClass('clip-number').text(start);
+		var dom_end = $('<div></div>').addClass('clip-time').text(end);
+		dom_sub.append(dom_start).append(dom_end);
+		dom_head.append(dom_sub);
+		dom_head.bind("click", function(){
+			// $(".cut_active").removeClass('cut_active');
+			set_selection($(this));
+		})
+		$('#video_cut_list').append(dom_head);
+	}
+
+	function set_selection(p){
+		var start = p.attr('data-start');
+		var end = p.attr('data-end');
+		mplayer.setValueSlider(start, end);
+		mplayer.pause();
+		mplayer.currentTime(start);
+		$(".cut_active").removeClass('cut_active');
+		p.addClass('cut_active')
+		// active = start
+	}
 	
     if(currentPanel !== '#' && currentPanel !== '') {
 	$('.nav-tab > .nav-square > li').removeClass('current');
