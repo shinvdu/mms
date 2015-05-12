a1b8c9f11811822246823d63b8dd64366516c6b8
wzdxt <wzdxt0624@gmail.com>
2015-05-13 03:33:28 +0800

    preview video product fix css
    Signed-off-by: wzdxt <wzdxt0624@gmail.com>

--- app/controllers/video_products_controller.rb
+++ app/controllers/video_products_controller.rb
@@ -3,4 +3,10 @@ class VideoProductsController < ApplicationController
     # send_file VideoProduct.find(params[:id]).get_m3u8_file_path
     redirect_to VideoProduct.find(params[:id]).video_detail.get_full_url
   end
+
+  def show
+    @video_product = VideoProduct.find(params[:id])
+    @video_type = File.extname(@video_product.video_detail.uri)[1..-1]
+  end
 end
+
