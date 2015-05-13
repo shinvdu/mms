6e6a81088b1fc1da273b45cb05a9dfa8efe693fe
wzdxt <wzdxt0624@gmail.com>
2015-05-13 13:36:34 +0800

    fix for html
    Signed-off-by: wzdxt <wzdxt0624@gmail.com>

--- config/initializers/basic_utils.rb
+++ config/initializers/basic_utils.rb
@@ -1,6 +1,6 @@
 module NumberUtils
   def to_time
-    "#{self.to_i/3600}:#{self.to_i%3600/60}:#{self-self.to_i/60*60}"
+    "#{self.to_i/3600}:#{self.to_i%3600/60}:#{(self-self.to_i/60*60).round(2)}"
   end
 end
 
