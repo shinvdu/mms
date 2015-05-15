e5de85f6a71ace90422673bd7a4b9b6a75208bac
silas <xie.tianbao44@gmail.com>
2015-05-15 13:42:08 +0800

    新的方法监听sass文件,兼容生产环境的sass

--- public/mms/sprocket.rb
+++ public/mms/sprocket.rb
@@ -1,35 +1,47 @@
 #!/usr/bin/env ruby
 require 'rubygems'
 require 'sprockets'
+require 'listen'
+
 if ARGV[0] == '-h' || ARGV[0] == '--help'
 	puts 'command [input_file] [output_file]'
 	exit 0
 end
-if not  (ARGV[0] && ARGV[1])
-	puts 'command [input_file] [output_file]'
-	exit 
-end
-
-input = ARGV[0]
-output = ARGV[1]
- 
 environment = Sprockets::Environment.new
 environment.append_path '.'
+environment.append_path './css/'
 
 environment.context_class.class_eval do
   def asset_path(path, options = {})
-  	if options[:type] == :image
-  		return     "'../images/#{path}'"
-  	end
-  	# "assets/#{path}"
-  	# "assets/#{path}"
-  	"'../#{options[:type]}s/#{path}'"
+    if options[:type] == :image
+      return     "'../images/#{path}'"
+    end
+    # "assets/#{path}"
+    # "assets/#{path}"
+    "'../#{options[:type]}s/#{path}'"
   end
 end
 
-File.delete output if File.exists?(output)
+def compile(env, input, output)
+  File.delete output if File.exists?(output)
+  bb = env.find_asset(input).to_s
+  File.open(output, 'w') { |file| file.write(bb) }
+end
 
-bb = environment.find_asset(input).to_s
+# compile(environment, 'main.scss', '/mnt/app/mms/public/mms/css/main.css')
+listener = Listen.to('.', './css/', only: /\.scss$/) do |modified, added, removed|
+  puts "compiling file: #{modified}"
+  filename = File.basename(modified[0], ".scss")
+  dir = File.dirname(modified[0])
+  input = "#{filename}.scss"
+  puts  "input: #{input} "
+  outfile = "#{dir}/#{filename}.css"
+  puts  "ouput: #{outfile}"
+  compile(input, outfile)
+  puts '-------------------------------------------------------------'
+  # puts "added absolute path: #{added}"
+  # puts "removed absolute path: #{removed}"
+end
 
-File.open(output, 'w') { |file| file.write(bb) }
-
+listener.start # not blocking
+sleep
