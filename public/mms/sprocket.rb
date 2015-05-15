01c6ff7348f1500f3a904c32c7d641619b3a6631
silas <xie.tianbao44@gmail.com>
2015-05-15 10:43:47 +0800

    sass文件自动解释器

--- public/mms/sprocket.rb
+++ public/mms/sprocket.rb
@@ -1,21 +1,29 @@
 #!/usr/bin/env ruby
 require 'rubygems'
 require 'sprockets'
+if ARGV[0] == '-h' || ARGV[0] == '--help'
+	puts 'command [input_file] [output_file]'
+	exit 0
+end
+if not  (ARGV[0] && ARGV[1])
+	puts 'command [input_file] [output_file]'
+	exit 
+end
 
-input = 'tian.scss' 
-output = 'tian.css'
+input = ARGV[0]
+output = ARGV[1]
  
 environment = Sprockets::Environment.new
 environment.append_path '.'
 
 environment.context_class.class_eval do
   def asset_path(path, options = {})
-  	puts options
   	if options[:type] == :image
-  		return     "images/#{path}"
+  		return     "'../images/#{path}'"
   	end
   	# "assets/#{path}"
-  	"assets/#{path}"
+  	# "assets/#{path}"
+  	"'../#{options[:type]}s/#{path}'"
   end
 end
 
