dfdcd6b91f7891d1587bf0be2495e959948c9b12
silas <xie.tianbao44@gmail.com>
2015-05-15 16:45:35 +0800

    增加单元测试的模块

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+describe User do
+  before do
+  end
+
+  context "when initialized" do
+    # it "should have default status is New" do
+      # expect(@user.uid).to eq(1)
+    # end
+
+    # it "should have default amount is 0" do
+    #   expect()@order.amount).to eq(0)
+    # end
+  end
+end
