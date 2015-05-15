63b794544ae030d5be1e763009735b64bc9e14e8
charliestras <charliestras@gmail.com>
2015-05-15 20:02:38 +0800

    修正剪辑视频的样式

--- public/mms/js/app.js
+++ public/mms/js/app.js
@@ -1,14 +1,4 @@
 'use strict'
-function element_lists() {
-    var container = []
-    $('#video_select_list').children().each(function(){
-	var p = $(this)
-	var start  = p.attr('data-start');
-	var end  = p.attr('data-end');
-	container.push({start: start, end: end})
-    })
-	return container;
-}
 
 var main = function() {
     var currentPanel = document.location.hash,
@@ -16,89 +6,6 @@ var main = function() {
 	    hidden: false
 	},
 	mplayer = ($('#video-display').length !== 0) ? videojs("video-display") : undefined,
-
-	getNewClip = function() {
-	    var clipStart = mplayer.getValueSlider().start,
-		clipEnd = mplayer.getValueSlider().end,
-		clipFormatStart = videojs.formatTime(clipStart),
-		clipFormatEnd = videojs.formatTime(clipEnd),
-		clipCanvas,
-		prevTime = mplayer.currentTime();
-
-	    mplayer.currentTime(mplayer.getValueSlider().start)
-		.on('seeked', function() {
-		    clipCanvas = $('<canvas width="75" height="75"></canvas>')[0];
-		    if(clipCanvas.getContext) {
-			clipCanvas.getContext('2d').drawImage(mplayer.player().el().firstChild, 0, 0, 75, 75);
-		    }
-		    else {
-		    }
-		    $('.video-clip-pill').removeClass('clip-active');
-		    $(clipCanvas).appendTo($('#video-clip-list'));
-/*
-		    $('<div/>', { 'class': 'video-clip-pill clip-active', 'data-start': clipStart, 'data-end': clipEnd })
-			.append($('<a/>', { 'class': 'add-clip', 'href': 'javascript:void(0)', 'text': '+' })
-				.on('click', selectClip))
-			.append($('<a/>', { 'class': 'remove-clip', 'href': 'javascript:void(0)', 'text': '-'  })
-				.on('click', removeClip))
-			.append($('<div/>')
-				.append($('<div/>', { 'class': 'clip-time-start', 'text': clipFormatStart }))
-				.append($('<div/>', { 'class': 'clip-time-end', 'text': clipFormatEnd }))
-			       )
-			.css('background-image', 'url(' + clipCanvas.toDataURL() + ')')
-			.on('click', setClipSelection)
-			.appendTo($('#video-clip-list'));
-*/
-		    mplayer.currentTime(prevTime).off('seeked');
-		    mplayer.setValueSlider(clipEnd, clipEnd + 60);
-		})
-	},
-
-	selectClip = function() {
-	    var element = $(this).parent(),
-		clipStart = element.attr('data-start'),
-		clipEnd = element.attr('data-end'),
-		clipFormatStart = videojs.formatTime(clipStart),
-		clipFormatEnd = videojs.formatTime(clipEnd);
-
-	    $('#video-clip-selected .video-clip-pill.clip-active').removeClass('clip-active');
-	    $('<div/>', { 'class': 'video-clip-pill clip-active', 'data-start': clipStart, 'data-end': clipEnd })
-		.append($('<a/>', { 'class': 'remove-clip', 'href': 'javascript:void(0)', 'text': '-'  })
-			.on('click', removeSelectedClip))
-		.append($('<div/>')
-			.append($('<div/>', { 'class': 'clip-time-start', 'text': clipFormatStart }))
-			.append($('<div/>', { 'class': 'clip-time-end', 'text': clipFormatEnd }))
-		       )
-		.css('background-image', element.css('background-image'))
-		.on('click', setClipSelection)
-		.appendTo($('#video-clip-selected'));
-
-	    event.stopPropagation();
-	},
-
-	removeClip = function () {
-	    var element = $(this).parent(),
-		clipStart = element.attr('data-start'),
-		clipEnd = element.attr('data-end');
-	    $('#video-clip-selected').children('[data-start="' + clipStart + '"]').remove();
-	    element.remove();
-	},
-	removeSelectedClip = function () {
-	    var element = $(this).parent();
-	    element.remove();
-	},
-
-	setClipSelection = function() {
-	    var element = $(this),
-		clipStart = element.attr('data-start'),
-		clipEnd = element.attr('data-end');
-	    
-	    mplayer.setValueSlider(clipStart, clipEnd);
-	    mplayer.pause();
-	    mplayer.currentTime(clipStart);
-	    $('.video-clip-pill').removeClass('clip-active');
-	    element.addClass('clip-active');
-	},
 
 	$frame = $('#clip-frame'),
 	$frameWrap = $frame.parent(),
@@ -110,17 +17,150 @@ var main = function() {
 	    mouseDragging: 1,
 	    touchDragging: 1,
 	    releaseSwing: 1,
-	    startAt: 3,
+	    startAt: 1,
 	    speed: 300,
 	    elasticBounds: 1,
 	    easing: 'easeOutExpo',
 
-//	    forward: $wrap.find('.forward'),
-//	    backward: $wrap.find('.backward'),
-	    prev: $frameWrap.find('#clip-prev'),
-	    next: $frameWrap.find('#clip-next'),
-//	    prevPage: $wrap.find('.prevPage'),
-//	    nextPage: $wrap.find('.nextPage')
+	    prev: $frameWrap.find('.clip-prev'),
+	    next: $frameWrap.find('.clip-next'),
+	},
+
+	getNewClip = function() {
+	    var clipCanvas,
+		newClipItem,
+		position,
+		clipStart = mplayer.getValueSlider().start,
+		clipEnd = mplayer.getValueSlider().end,
+		clipDuration = Math.floor(clipEnd) - Math.floor(clipStart),
+		clipFormatStart = videojs.formatTime(clipStart),
+		clipFormatEnd = videojs.formatTime(clipEnd),
+		clipFormatDuration = videojs.formatTime(clipDuration),
+		prevTime = mplayer.currentTime(),
+		clipStartList = $.map($('#clip-frame li'), function(element) {
+		    return Number($(element).attr('data-start'));
+		});
+
+	    for(var i = clipStartList.length - 1; i >= 0; i--) {
+		if(clipEnd <= clipStartList[i]) {
+		    if(i === 0) {
+			position = 0;
+			break;
+		    }
+		    continue;
+		}
+		else {
+		    position = i + 1;
+		    break;
+		}
+	    }
+
+	    mplayer.currentTime(mplayer.getValueSlider().start)
+		.on('seeked', function() {
+		    clipCanvas = $('<canvas width="74" height="48"></canvas>')[0];
+		    if(clipCanvas.getContext) {
+			clipCanvas.getContext('2d').drawImage(mplayer.player().el().firstChild, 0, 0, 74, 48);
+		    }
+		    else {
+		    }
+		    $('#clip-frame li').removeClass('active');
+		    newClipItem = $('<li/>', { 'data-start': clipStart, 'data-end': clipEnd })
+			.append($('<div/>', { 'class': 'clip-canvas-wraper' })
+				.append($(clipCanvas))
+				.append($('<div/>')
+					.append($('<button/>', { 'class': 'add-clip' })
+						.append($('<i/>', { 'class': 'fa fa-plus' }))
+						.on('click', selectClip)
+					       )
+					.append($('<button/>', { 'class': 'remove-clip' })
+						.append($('<i/>', { 'class': 'fa fa-minus' }))
+						.on('click', removeClip)
+					       )
+				       )
+			       )
+			.append($('<div/>', { 'class': 'clip-info' })
+				.append($('<span/>', { 'text': clipFormatStart }))
+				.append('-')
+				.append($('<span/>', { 'text': clipFormatEnd }))
+				.append($('<br>'))
+				.append($('<span/>', { 'text': clipFormatDuration }))
+			       )
+			.on('click', setClipSelection)[0];
+		    $frame.sly('add', newClipItem, position);
+		    $frame.sly('activate', newClipItem);
+		    
+		    mplayer.currentTime(prevTime).off('seeked');
+		    mplayer.setValueSlider(clipEnd, clipEnd + 10);
+		})
+	},
+
+	selectClip = function() {
+	    var element = $(this).closest('li'),
+		clipStart = element.attr('data-start'),
+		clipEnd = element.attr('data-end'),
+		clipDuration = Math.floor(clipEnd) - Math.floor(clipStart),
+		clipFormatStart = videojs.formatTime(clipStart),
+		clipFormatEnd = videojs.formatTime(clipEnd),
+		clipFormatDuration = videojs.formatTime(clipDuration),
+		clipCanvas = $('<canvas width="74" height="48"></canvas>')[0];
+
+	    if(clipCanvas.getContext) {
+		clipCanvas.getContext('2d').drawImage(element.find('canvas')[0], 0, 0, 74, 48);
+	    }
+	    else {
+	    }
+
+	    $('#video-clip-merge .active').removeClass('active');
+	    $('<div/>', { 'class': 'video-clip-pill active', 'data-start': clipStart, 'data-end': clipEnd })
+		.append($('<div/>', { 'class': 'clip-canvas-wraper' })
+			.append($(clipCanvas))
+			.append($('<div/>')
+				.append($('<button/>', { 'class': 'remove-clip' })
+					.append($('<i/>', { 'class': 'fa fa-minus' }))
+					.on('click', removeSelectedClip)
+				       )
+			       )
+		       )
+		.append($('<div/>', { 'class': 'clip-info' })
+			.append($('<span/>', { 'text': clipFormatStart }))
+			.append('-')
+			.append($('<span/>', { 'text': clipFormatEnd }))
+			.append($('<br>'))
+			.append($('<span/>', { 'text': clipFormatDuration }))
+		       )
+		.on('click', setClipSelection)
+		.appendTo($('#video-clip-merge'));
+	    
+	    event.stopPropagation();
+	},
+
+	removeClip = function () {
+	    var element = $(this).closest('li'),
+		clipStart = element.attr('data-start'),
+		clipEnd = element.attr('data-end');
+	    $('#video-clip-merge').children('[data-start="' + clipStart + '"]').remove();
+	    $frame.sly('remove', element);
+	    event.stopPropagation();
+	},
+
+	removeSelectedClip = function () {
+	    var element = $(this).closest('.video-clip-pill');
+	    element.remove();
+	    event.stopPropagation();
+	},
+
+	setClipSelection = function() {
+	    var element = $(this),
+		clipStart = element.attr('data-start'),
+		clipEnd = element.attr('data-end');
+	    
+	    mplayer.setValueSlider(clipStart, clipEnd);
+	    mplayer.pause();
+	    mplayer.currentTime(clipStart);
+	    $('#clip-frame li').removeClass('active');
+	    $('#video-clip-merge .active').removeClass('active');
+	    element.addClass('active');
+	    event.stopPropagation();
 	};
 
     $frame.sly(slyOptions);
