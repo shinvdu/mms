d04477909bef44d76d57ba2b63239d6b0bc4e5ee
silas <xie.tianbao44@gmail.com>
2015-05-18 13:06:29 +0800

    发送确认邮件

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,7 +1,7 @@
 class Account < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,  :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:login]
+  devise :database_authenticatable, :registerable,  :recoverable, :rememberable, :trackable, :validatable, :confirmable, :authentication_keys => [:login]
   attr_accessor :login
   belongs_to :user, foreign_key: :user_id
   validates_uniqueness_of :username
