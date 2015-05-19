c62b4cb5c4c8f7d8456cf599de787db263e5b4ac
silas <xie.tianbao44@gmail.com>
2015-05-19 21:26:11 +0800

    bb

---
+++ spec/supports/devise.rb
@@ -0,0 +1,20 @@
+#module ControllerDevise
+  def login_user
+    before do
+      single_login_user
+    end
+  end
+
+  def single_login_user
+    request.env["devise.mapping"] = Devise.mappings[:user]
+    @account = FactoryGirl.create(:account)
+    sign_in @user
+  end
+
+  def relogin_user2
+    sign_out @user
+    request.env["devise.mapping"] = Devise.mappings[:user]
+    @user2 = FactoryGirl.create(:zhang)
+    sign_in @user2
+  end
+#end
