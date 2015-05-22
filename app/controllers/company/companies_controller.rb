7778ffbb0bb54061409a56ad755a633c3f34e572
wzdxt <wzdxt0624@gmail.com>
2015-05-22 19:06:25 +0800

    triger rollback
    Signed-off-by: wzdxt <wzdxt0624@gmail.com>

--- app/controllers/company/companies_controller.rb
+++ app/controllers/company/companies_controller.rb
@@ -13,20 +13,23 @@ class Company::CompaniesController < ApplicationController
   end
 
   def create
-    ActiveRecord::Base.transaction do
-      @company_account = Account.new(company_account_params.except(:user))
-      @company_account.user = User.new(company_account_params[:user].except(:company))
-      @company_account.user.role = Settings.role.company_owner
-      @company_account.user.company = Company.new(company_account_params[:user][:company])
-      @company_account.username = @company_account.user.nickname
-      respond_to do |format|
-        if @company_account.save
-          format.html { redirect_to companies_path, notice: 'Company owner was successfully created.' }
-          format.json { render :show, status: :created, location: @company_account }
-        else
-          format.html { render :new }
-          format.json { render json: @company_account.errors, status: :unprocessable_entity }
+    begin
+      ActiveRecord::Base.transaction do
+        @company_account = Account.new(company_account_params.except(:user))
+        @company_account.user = User.new(company_account_params[:user].except(:company))
+        @company_account.user.role = Settings.role.company_owner
+        @company_account.user.company = Company.new(company_account_params[:user][:company])
+        @company_account.username = @company_account.user.nickname
+        @company_account.save!
+        respond_to do |format|
+            format.html { redirect_to companies_path, notice: 'Company owner was successfully created.' }
+            format.json { render :show, status: :created, location: @company_account }
         end
+      end
+    rescue
+      respond_to do |format|
+        format.html { render :new }
+        format.json { render json: @company_account.errors, status: :unprocessable_entity }
       end
     end
   end
