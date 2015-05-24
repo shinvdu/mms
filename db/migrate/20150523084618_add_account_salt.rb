c8ab4a85a0162af8d200ba5b0e4ed535a9127569
silas <xie.tianbao44@gmail.com>
2015-05-24 16:32:43 +0800

    bug fix 不能全新运行migration的问题

--- db/migrate/20150523084618_add_account_salt.rb
+++ db/migrate/20150523084618_add_account_salt.rb
@@ -1,5 +1,5 @@
 class AddAccountSalt < ActiveRecord::Migration
   def change
-  	add_column :accounts, :password_salt, :string, after: :encrypted_password
+  	add_column :accounts, :password_salt, :string, after: :encrypted_password  if !(Account.column_names.include?('password_salt'))
   end
 end
