828aa8696341005dd6e8b9bc542b019e86a2a997
silas <xie.tianbao44@gmail.com>
2015-05-28 13:48:33 +0800

    修复全新运行migration的错误

--- db/migrate/20150420080327_create_provider_auths.rb
+++ db/migrate/20150420080327_create_provider_auths.rb
@@ -1,13 +1,15 @@
 class CreateProviderAuths < ActiveRecord::Migration
   def change
-    create_table :provider_auths do |t|
-      t.integer :user_id
-      t.integer :account_id
-      t.string :provider_uid
-      t.string :provider
-      t.boolean :status, default: true
+    if not ActiveRecord::Base.connection.table_exists? 'provider_auths'
+      create_table :provider_auths do |t|
+        t.integer :user_id
+        t.integer :account_id
+        t.string :provider_uid
+        t.string :provider
+        t.boolean :status, default: true
 
-      t.timestamps null: false
+        t.timestamps null: false
+      end
     end
   end
 end
