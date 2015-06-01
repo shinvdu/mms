b87e914898cee59f2a75fc8fce81f029c3313009
silas <xie.tianbao44@gmail.com>
2015-06-01 15:20:09 +0800

    fatory girl

--- spec/controllers/video_controller_spec.rb
+++ spec/controllers/video_controller_spec.rb
@@ -5,8 +5,12 @@ RSpec.describe VideoController, type: :controller do
   describe "GET #show" do
     it "returns http success" do
       video_group = FactoryGirl.create(:video_product_group)
-      video_detail = FactoryGirl.create(:video_detail)
-      video_product = FactoryGirl.create(:video_product)
+      video_detail_1 = FactoryGirl.create(:video_detail)
+      transcoding_1 = FactoryGirl.create(:transcoding)
+      video_product_1 = FactoryGirl.create(:video_product, video_detail: video_detail_1, video_product_group: video_group, transcoding: transcoding_1)
+      video_detail_2 = FactoryGirl.create(:video_detail)
+      transcoding_2 = FactoryGirl.create(:transcoding)
+      video_product_2 = FactoryGirl.create(:video_product, video_detail: video_detail_2, video_product_group: video_group, transcoding: transcoding_2)
       get :show, :id =>  video_group.id
       expect(response).to have_http_status(:success)
     end
