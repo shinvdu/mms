9358da095b125544b00e305f05353bc646329596
silas <xie.tianbao44@gmail.com>
2015-06-03 09:20:55 +0800

    backkup

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -1,4 +1,6 @@
 class VideoController < ApplicationController
+  before_action :explode_session_id
+
   def show
     @video_group = VideoProductGroup.find params[:id]
     if @video_group.FINISHED? && @video_group.ACCEPTED?
@@ -21,6 +23,20 @@ end
 
 def iframe
 
+end
+
+def get_video_real_address 
+  # params: video_id = ? 
+  # params: signture = ? 
+  # params: md5 = ? 
+
+end
+
+protected
+
+def explode_session_id
+  cookies[:session_id] = session[:session_id]
+  cookies[:video_salt] = Settings.video_salt
 end
 
 end
