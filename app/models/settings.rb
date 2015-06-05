b71fa7fbfcc1d6c44d375b44bbc996691c6eadf5
silas <xie.tianbao44@gmail.com>
2015-06-05 11:00:43 +0800

    程序配置和帐号配置分离

--- app/models/settings.rb
+++ app/models/settings.rb
@@ -1,4 +1,11 @@
 class Settings < Settingslogic
-  source "#{Rails.root}/config/application.yml"
-  namespace Rails.env
+	source "#{Rails.root}/config/application.yml"
+	namespace Rails.env
+
+	if File.exist?("#{Rails.root}/config/wgconfig.yml")
+		# logger.info '===> Local application configuration file loaded.'
+		instance.deep_merge!(Settings.new("#{Rails.root}/config/wgconfig.yml"))  
+	else
+		# logger.error '===> Local application configuration file no found.'
+	end
 end
--- app/models/wgconfigs.rb
+++
@@ -1,4 +0,0 @@
-class Wgconfigs < Settingslogic
-  source "#{Rails.root}/config/program_config.yml"
-  namespace Rails.env
-end
