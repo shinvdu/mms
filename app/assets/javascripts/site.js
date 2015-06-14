15c1a2284a690ef49a45f4e375645dc7f9ba6ed6
silas <silas@sky-city.me>
2015-06-14 17:37:16 +0800

    把多文件上传js库移到库目录

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require lib/jquery.min
-//= require lib/jquery-ui.min
+//= require lib/jquery/jquery.min
+//= require lib/jquery/jquery-ui.min
 //= require jquery_ujs
 //= require turbolinks
 //= require lib/sly.min
